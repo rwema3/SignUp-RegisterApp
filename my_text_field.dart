28441a1374159532f66aec1cdb407ad6f25e0f87
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-30 19:12:32 +0200

    Update my_text_field.dart

--- my_text_field.dart
+++ my_text_field.dart
@@ -14,4 +14,4 @@ class MyTextField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 1
+      padding: const EdgeInsets.symmetric(vertical: 10
