5198c1aee189af69165f0514aac08beb6ede8420
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-30 19:37:01 +0200

    Update my_text_field.dart

--- my_text_field.dart
+++ my_text_field.dart
@@ -28,4 +28,4 @@ class MyTextField extends StatelessWidget {
               color: Colors.grey,
               width: 1,
             ),
-            borderRadius: BorderRadius.c
+            borderRadius: BorderRadius.ci
