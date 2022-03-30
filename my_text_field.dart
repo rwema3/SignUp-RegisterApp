87bc2136c3833615d976074bd5f79b9f10b29e85
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-30 19:33:39 +0200

    Update my_text_field.dart

--- my_text_field.dart
+++ my_text_field.dart
@@ -24,4 +24,4 @@ class MyTextField extends StatelessWidget {
           hintText: hintText,
           hintStyle: kBodyText,
           enabledBorder: OutlineInputBorder(
-            borderSide: Borde
+            borderSide: Border
