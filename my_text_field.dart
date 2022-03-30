1d9beed271375708278f5ec4e8aa4b45b7fc4e5f
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-30 19:23:17 +0200

    Update my_text_field.dart

--- my_text_field.dart
+++ my_text_field.dart
@@ -19,4 +19,4 @@ class MyTextField extends StatelessWidget {
         style: kBodyText.copyWith(color: Colors.white),
         keyboardType: inputType,
         textInputAction: TextInputAction.next,
-        decoratio
+        decoration
