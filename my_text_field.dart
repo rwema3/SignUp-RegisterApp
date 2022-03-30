d593ca5084c0e71cc190016290524deba1241189
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-30 17:41:45 +0200

    Update my_text_field.dart

--- my_text_field.dart
+++ my_text_field.dart
@@ -16,4 +16,4 @@ class MyTextField extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 10),
       child: TextField(
-        style: kBodyText.copyWith(color: Col
+        style: kBodyText.copyWith(color: Co
