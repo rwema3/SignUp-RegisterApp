c763badaa075ab9debd21417a976dc1f97293196
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-30 17:32:03 +0200

    Update my_text_field.dart

--- my_text_field.dart
+++ my_text_field.dart
@@ -20,4 +20,4 @@ class MyTextField extends StatelessWidget {
         keyboardType: inputType,
         textInputAction: TextInputAction.next,
         decoration: InputDecoration(
-          contentPadding: EdgeIn
+          contentPadding: EdgeI
